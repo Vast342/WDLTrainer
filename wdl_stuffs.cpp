794fee83cd21571e39207c4a14de86262c04067f
Vast342 <78440113+Vast342@users.noreply.github.com>
2024-02-05 09:28:52 -0500

    Neatening

--- wdl_stuffs.cpp
+++ wdl_stuffs.cpp
@@ -30,22 +30,6 @@ struct DataPoint {
 std::string pgnDir;
 std::vector<DataPoint> win_rate_data;
 
-// splits a string into segments based on the seperator
-// borrowed from Clarity
-std::vector<std::string> split(const std::string string, const char seperator) {
-    std::stringstream stream(string);
-    std::string segment;
-    std::vector<std::string> list;
-
-    // every time that it can get a segment
-    while(std::getline(stream, segment, seperator)) {
-        // add it to the vector
-        list.push_back(segment);
-    }
-    
-    return list;
-}
-
 // initial values borrowed from stockfish
 std::array<double, 8> params = {-1.719, 12.448, -12.855, 331.883, -3.001, 22.505, -51.253, 93.209};
 
@@ -72,19 +56,6 @@ std::array<double, 3> calculate_wdl(int score, int move_count) {
     return {win_ratio, draw_ratio, loss_ratio};
 }
 
-// round number to nearest multiple of 5
-int round(int n) 
-{ 
-    // Smaller multiple 
-    int a = (n / 5) * 5; 
-      
-    // Larger multiple 
-    int b = a + 5; 
-  
-    // Return of closest of two 
-    return (n - a > b - n)? b : a; 
-} 
-
 struct Case
 {
     int moveCount, score;
@@ -110,32 +81,25 @@ void convert_data() {
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - beginTime).count();
     std::cout << "File loaded in " << elapsedTime << " ms, total of " << lineCount << " lines" << std::endl;
     beginTime = std::chrono::steady_clock::now();
-    struct
-    {
-        bool operator()(Case a, Case b) const {
-            if (a.moveCount < b.moveCount)
-                return true;
-            if (a.moveCount > b.moveCount)
-                return false;
-            // a.first == b.first, compare second
-            return a.score < b.score;
-         }
-    }
-    customLessThan;
+    // sort them using the custom comparison above
+    // The custom comparison above makes it so that they are sorted first by the move number, then by the score. this makes it easier to loop through later.
     std::sort(cases.begin(), cases.end(), customLessThan);
+    // std::sort is insanely quick, before enabling compiler optimisations it took nearly 10 minutes to sort 1.3m points of data, with std::sort it took about half a second.
 
     elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - beginTime).count();
     std::cout << "Sorted cases in " << elapsedTime << " ms" << std::endl;
     beginTime = std::chrono::steady_clock::now();
+    
     // convert each data point to a winrate based on move count and score
     int currentMove = 0;
     int currentScore = -10000;
     int currentScoreInstances = 0;
     int currentScoreWins = 0;
     int number = 0;
+    // loops through the points of data
     for(Case entry : cases) {
         number++;
-        //std::cout << "entry number " << number << " current move: " << currentMove << " current score: " << currentScore << std::endl;
+        // for each score, it adds up the total wins and the total times that score has appeared at that move count, and from there the win rate is simply the wins / instances.
         if(currentMove != entry.moveCount) {
             currentMove = entry.moveCount;
             currentScore = -10000;
@@ -144,6 +108,7 @@ void convert_data() {
         }
         if(currentScore != entry.score) {
             if(currentScore != -10000) {
+                // cast those to doubles so that it's a double now, and gets divided with the double's level of precision.
                 win_rate_data.push_back(DataPoint(currentMove, currentScore, double(currentScoreWins) / double(currentScoreInstances)));
             }
             currentScore = entry.score;
@@ -170,22 +135,26 @@ double calculate_error() {
 
 void train_stuff() {
     std::cout << "initial error: " << calculate_error() << std::endl;
-    std::cout << "what would you like the learning rate to start out as?" << std::endl;
-    double lr;
-    std::cin >> lr;
     std::cout << "how many iterations would you like?" << std::endl;
     int iterations;
     std::cin >> iterations;
     for(int i = 0; i < iterations; i++) {
         // stuff to do each iteration
+        // calculate gradient
+        // adjust accordingly
+        // test error again.
     }
     std::cout << "this is totally me training stuff" << std::endl;
     std::cout << "final error: " << calculate_error() << std::endl;
 }
 
 void output_params() {
-    std::cout << "here are your parameters:" << std::endl;
-    std::cout << "lmao sike i didn't do ANYTHING yet" << std::endl;
+    std::cout << "Current parameter state:" << std::endl;
+    std::cout << "std::array<double, 8> wdlParams = {";
+    for(double param : params) {
+        std::cout << param << ", ";
+    }
+    std::cout << "};" << std::endl;
 }
 
 int main() {
